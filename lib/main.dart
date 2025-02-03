2f37a0201cfe3f8f7ed0f3801b65e41bdbfa8b89
Mohamed <msanoko5@gmail.com>
2025-02-03 14:02:05 -0500

    Formatted dart files

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,10 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   void _onBuildingSelectionChanged(String newSelection) {
     print('Building selected: $newSelection');
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,7 +19,10 @@ class MyApp extends StatelessWidget {
           title: Text('Testing building switch button'),
         ),
         body: Center(
-          child: BuildingSwitch(onSelectionChanged: _onBuildingSelectionChanged, initialSelection: 'SGW',),
+          child: BuildingSwitch(
+            onSelectionChanged: _onBuildingSelectionChanged,
+            initialSelection: 'SGW',
+          ),
         ),
       ),
     );
