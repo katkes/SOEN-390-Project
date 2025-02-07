d9cc2fb021374c028f9d85b1f4cd863f9ebe7e84
JJCrete <jjcrete12@icloud.com>
2025-02-06 19:15:33 -0500

    search bar
    added search bar widget

--- lib/widgets/search_bar.dart
+++ lib/widgets/search_bar.dart
@@ -1,1 +1,101 @@
+import 'package:flutter/material.dart';
 
+class SearchBarWidget extends StatefulWidget {
+  final TextEditingController controller;
+  final Function(String)? onChanged;
+
+  const SearchBarWidget({Key? key, required this.controller, this.onChanged})
+      : super(key: key);
+
+  @override
+  _SearchBarWidgetState createState() => _SearchBarWidgetState();
+}
+
+class _SearchBarWidgetState extends State<SearchBarWidget> {
+  bool isExpanded = false;
+  FocusNode _focusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode.addListener(() {
+      setState(() {
+        isExpanded = _focusNode.hasFocus;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Align(
+          alignment: Alignment.bottomLeft,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 20, bottom: 90),
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  isExpanded = !isExpanded;
+                  if (isExpanded) {
+                    _focusNode.requestFocus();
+                  } else {
+                    _focusNode.unfocus();
+                  }
+                });
+              },
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                width: isExpanded ? 280 : 48,
+                height: 40,
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(24),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color(0x3F000000),
+                      blurRadius: 10,
+                      offset: Offset(0, 0),
+                    ),
+                  ],
+                ),
+                child: Row(
+                  children: [
+                    Icon(Icons.search, color: Colors.grey.withOpacity(0.7)),
+                    if (isExpanded) ...[
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: TextField(
+                          focusNode: _focusNode,
+                          controller: widget.controller,
+                          onChanged: widget.onChanged,
+                          decoration: InputDecoration(
+                            hintText: 'Search',
+                            hintStyle: TextStyle(
+                              color: Colors.black.withOpacity(0.3),
+                              fontSize: 16,
+                              fontFamily: 'Inter',
+                              fontWeight: FontWeight.w400,
+                            ),
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
