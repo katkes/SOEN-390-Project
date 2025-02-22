a1f2b456a9a0fd3e0a8ce57ec671914a5b664cc6
mohamedsanoko <msanoko5@gmail.com>
2025-02-22 11:15:57 -0500

    deleting backend folder and adding test file for building details retrieval

--- backend/project/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from route.services.consumers import LiveRouteConsumer
-
-websocket_urlpatterns = [
-    re_path(r"ws/routes/live/$", LiveRouteConsumer.as_asgi()),  # 🔥 WebSocket route
-]
