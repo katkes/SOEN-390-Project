8763683af3f1321a27664c7d9ad0047f204d6686
ArnabDasSOEN <arnabdas1234@hotmail.com>
2025-02-22 15:12:52 -0500

    small touches for clean up

--- frontend/lib/utils/user_location_util.dart
+++ frontend/lib/utils/user_location_util.dart
@@ -1,6 +1,9 @@
 import 'package:flutter_background_geolocation/flutter_background_geolocation.dart' as bg;
 import 'package:flutter/material.dart'; //need to import this all the time because flutter classes like statefulwidget dont exist naturally in flutter. You need to import it. You're coding in dart, not flutter
 
+
+
+
 //This file is to obtain the current location of the user.
 // Do not access any location specific services until the .ready(config) method is done running.
 
@@ -9,16 +12,14 @@ class LocationService {
 
   // If you are using this service, this is the sole variable you care about. Just access this.
   bg.Location? currentLocation;
+
+  //instance of the singleton pattern.
   static final LocationService _instance = LocationService._internal();
 
   //this is the private constructor.
   LocationService._internal();
 
-  // //constructor
-  // _LocationService() {
-  //   return location_service;
-  // }
-
+  //factory to give out instance.
   factory LocationService() {
     return _instance;
   }
@@ -26,12 +27,10 @@ class LocationService {
   void getUserLocation(){
 
     //listen to events here
-
     // Fired whenever a location is recorded
     bg.BackgroundGeolocation.onLocation((bg.Location location) {
       currentLocation = location;
     });
-
     // Fired whenever the plugin changes motion-state (stationary->moving and vice-versa)
     bg.BackgroundGeolocation.onMotionChange((bg.Location location) {
       currentLocation = location;
