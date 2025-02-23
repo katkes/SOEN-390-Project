25dee32ad589854004e3ddbd2493f93eba482e4b
KapJ1coH <tima.mtl@gmail.com>
2025-02-23 14:59:40 -0500

    removed unused report

--- frontend/lib/widgets/outdoor_map.dart
+++ frontend/lib/widgets/outdoor_map.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-import 'dart:math' as math;
 
 import 'package:soen_390/services/route_service.dart';
 // ignore: depend_on_referenced_packages
