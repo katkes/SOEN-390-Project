9a818d395fc33036cdfc3f6cb2d98958e3331a10
mohamedsanoko <msanoko5@gmail.com>
2025-03-01 15:39:16 -0500

    Integration test functional

--- frontend/integration_test/app_test.dart
+++ frontend/integration_test/app_test.dart
@@ -1,25 +1,45 @@
-import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test_helper/integration_test_helper.dart';
+import 'package:integration_test/integration_test.dart';
 import 'package:soen_390/main.dart' as app;
+import 'package:soen_390/widgets/outdoor_map.dart';
 
 void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
   group('App Test', () {
-    IntegrationTestHelperBinding.ensureInitialized();
     testWidgets('Switching between campuses and viewing building info when clicking on a building', (tester) async{
       app.main();
       await tester.pumpAndSettle(); // waiting for the app to settle
+      await Future.delayed(const Duration(seconds: 2));
 
-      // TODO: Create finders for toggle button, navigation bar, and buildings
+      // Finding the map icon in navbar and tapping on it to switch to map section
+      final mapIconFinder = find.byIcon(Icons.map_outlined);
+      await tester.tap(mapIconFinder);
+      await tester.pumpAndSettle();
+      await Future.delayed(const Duration(seconds: 2));
+
+      // Finding the campuses toggle buttons
       final sgwToggleButton = find.text('SGW').first; // Retrieves first element it sees
       final loyolaToggleButton = find.text('Loyola').first; // Retrieves first element it sees
 
-      // TODO: Tap on the toggle button and wait till it settled
+      // Tapping on loyola and waiting till it settled
+      await tester.tap(loyolaToggleButton);
+      await tester.pumpAndSettle();
+      await Future.delayed(const Duration(seconds: 2));
+
+      // Verifying that the loyola campus is being rendered
+      expect(find.byType(MapWidget), findsOneWidget);
+
+      // Tapping on sgw and waiting till it settled
       await tester.tap(sgwToggleButton);
       await tester.pumpAndSettle();
-      // TODO: Check that the appropriate map is being rendered depending on the campus
+      await Future.delayed(const Duration(seconds: 2));
+
+      // Verifying that the sgw campus is being rendered
+      expect(find.byType(MapWidget), findsOneWidget);
+
       // TODO: Tap on a building and wait till it settled
       // TODO: Check that the appropriate building info is being rendered
       // TODO: Check that the navigation bar is being rendered
