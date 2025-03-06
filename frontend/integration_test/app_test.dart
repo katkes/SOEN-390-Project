ad91ed37cb8d214bd5b0b9ca24a933570d575a01
mohamedsanoko <msanoko5@gmail.com>
2025-03-05 22:13:58 -0500

    addition of github action to manually trigger execution of integration test on github

--- frontend/integration_test/app_test.dart
+++ frontend/integration_test/app_test.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:soen_390/main.dart' as app;
 import 'package:soen_390/widgets/outdoor_map.dart';
+import 'package:soen_390/widgets/route_card.dart';
 import 'dart:math';
 
+// The purpose of this test is to automate user interactions with the app's map and navigation features, ensuring they function correctly in a real or emulated environment.
+// It simulates user behaviors to verify that the app behaves as expected in different scenarios.
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  // Finding the map icon in navbar and tapping on it to switch to map section
+  // Function to find the map icon in navbar and tapping on it to switch to map section
   Future<void> navigatingToMapSection(WidgetTester tester) async {
     final mapIconFinder = find.byIcon(Icons.map_outlined);
     await tester.tap(mapIconFinder);
@@ -17,40 +19,46 @@ void main() {
     await Future.delayed(const Duration(seconds: 3));
   }
 
-  // Loading the app for testing
+  // Function to load the app for testing
   Future<void> loadingApp(WidgetTester tester) async {
     app.main();
     await tester.pumpAndSettle();
     await Future.delayed(const Duration(seconds: 3));
   }
 
-  Future<void> selectLocation(WidgetTester tester, String location) async {
-    final startLocationFinder = find.text('Start Location');
+  // Function to enter start address and destination address
+  Future<void> selectLocation(
+      WidgetTester tester, String option, String location) async {
+    final startLocationFinder = find.text(option);
     await tester.tap(startLocationFinder);
     await tester.pumpAndSettle();
     await Future.delayed(const Duration(seconds: 3));
-    final searchBarFinder = find.text("Enter an address");
+    final searchBarFinder = find.text("Type address...");
     await tester.tap(searchBarFinder, warnIfMissed: false);
     await tester.pumpAndSettle();
     final typeAddressFinder = find.byType(TextField);
     await tester.enterText(typeAddressFinder, location);
     await tester.pumpAndSettle();
-    await Future.delayed(const Duration(seconds: 3));
+    await Future.delayed(const Duration(seconds: 5));
     final confirmAddressFinder = find.text("Use this Address");
     await tester.tap(confirmAddressFinder);
     await tester.pumpAndSettle();
     await Future.delayed(const Duration(seconds: 3));
   }
 
-
+  // Group containing all the test flows of the app
   group('App Test', () {
-    testWidgets('Testing ability to switch campuses, sections, and building pop-ups', (tester) async{
+    testWidgets(
+        'Testing ability to switch campuses, sections, and building pop-ups',
+        (tester) async {
       await loadingApp(tester);
       await navigatingToMapSection(tester);
 
       // Finding the campuses toggle buttons
-      final sgwToggleButton = find.text('SGW').first; // Retrieves first element it sees
-      final loyolaToggleButton = find.text('Loyola').first; // Retrieves first element it sees
+      final sgwToggleButton =
+          find.text('SGW').first; // Retrieves first element it sees
+      final loyolaToggleButton =
+          find.text('Loyola').first; // Retrieves first element it sees
 
       // Tapping on loyola and waiting till it settled
       await tester.tap(loyolaToggleButton);
@@ -90,18 +98,22 @@ void main() {
     testWidgets('Testing transportation process', (tester) async {
       await loadingApp(tester);
       await navigatingToMapSection(tester);
+      expect(find.byType(MapWidget), findsOneWidget);
 
       // Navigating to transportation screen
       final findMyWayButton = find.text('Find My Way');
       await tester.tap(findMyWayButton);
       await tester.pumpAndSettle();
       await Future.delayed(const Duration(seconds: 3));
+      expect(find.text('Start Location'), findsOneWidget);
 
       // Selecting start location
-      await selectLocation(tester, "Hall Building");
+      await selectLocation(tester, "Start Location",
+          "Hall Building Auditorium, Boulevard De Maisonneuve Ouest, Montreal, QC, Canada");
 
       // Selecting destination
-      await selectLocation(tester, "Stingers Dome");
+      await selectLocation(tester, "Destination",
+          "Stinger Dome, Sherbrooke Street West, Montreal, QC, Canada");
 
       // Selecting car transportation option
       final carButton = find.byIcon(Icons.directions_car);
@@ -114,6 +126,9 @@ void main() {
       await tester.tap(confirmRouteButton);
       await tester.pumpAndSettle();
       await Future.delayed(const Duration(seconds: 3));
+
+      // Checks for displaying of routes
+      expect(find.byType(RouteCard), findsWidgets);
     });
   });
 }
