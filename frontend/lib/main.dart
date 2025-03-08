2a8335ca815e8fc76bd60a5a758c1c199b6b8657
sydneyC678 <sydcampbell@hotmail.com>
2025-03-08 01:06:25 -0500

    Adding the initialization and definition of the location service, creating new variable to store user's live location

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import 'package:soen_390/screens/waypoint/waypoint_selection_screens.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:soen_390/widgets/nav_bar.dart';
@@ -8,12 +9,13 @@ import 'package:soen_390/widgets/campus_switch_button.dart';
 import 'package:soen_390/widgets/indoor_navigation_button.dart';
 import 'package:soen_390/widgets/outdoor_map.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:soen_390/providers/service_providers.dart'; // Import providers
-import 'package:soen_390/services/http_service.dart'; // Import HttpService
-import 'package:soen_390/services/interfaces/route_service_interface.dart'; // Import IRouteService
+import 'package:soen_390/providers/service_providers.dart'; 
+import 'package:soen_390/services/http_service.dart'; 
+import 'package:soen_390/services/interfaces/route_service_interface.dart'; 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:soen_390/services/building_info_api.dart';
-import "package:soen_390/utils/location_service.dart";
+import 'package:soen_390/utils/location_service.dart';
+
 
 /// The entry point of the application.
 ///
@@ -26,7 +28,6 @@ void main() async {
   } catch (e) {
     print("An error occurred when loading the ..env file: $e");
   }
-
   runApp(
     const ProviderScope(
       child: MyApp(),
@@ -95,22 +96,40 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
 
   /// The user's current location on the map.
   LatLng _currentLocation = const LatLng(45.497856, -73.579588);
+  LatLng _userLiveLocation = const LatLng(5.497856, -73.579588);
+
   // http.Client? _httpClient;
   late BuildingPopUps _buildingPopUps;
   late GoogleMapsApiClient _mapsApiClient;
+
+  late LocationService _locationService;
 
   @override
   void initState() {
     super.initState();
     _mapsApiClient = GoogleMapsApiClient(
         apiKey: dotenv.env['GOOGLE_MAPS_API_KEY']!,
-        client: widget.httpService.client);
+        client: widget.httpService.client as BaseClient);
     _buildingPopUps = BuildingPopUps(mapsApiClient: _mapsApiClient);
+
+    //This initializes the location service and listens for updates
+    _locationService = LocationService.instance;
+    _locationService.startUp().then((_) {
+      _locationService.getLatLngStream().listen((LatLng location) {
+        setState(() {
+          _userLiveLocation = location;
+          print("User live location updated: $location");
+        });
+      });
+    }).catchError((e) {
+      print("Error starting location service: $e");
+    });
   }
 
-  @override
+  @override 
   void dispose() {
     super.dispose();
+    _locationService.stopListening();
   }
 
   void _onItemTapped(int index) {
@@ -182,6 +201,7 @@ class _MyHomePageState extends ConsumerState<MyHomePage> {
                           borderRadius: BorderRadius.circular(30),
                           child: MapWidget(
                             location: _currentLocation,
+                            userLocation: _userLiveLocation,
                             routeService: widget.routeService,
                             httpClient: widget.httpService.client,
                             mapsApiClient: _mapsApiClient,
