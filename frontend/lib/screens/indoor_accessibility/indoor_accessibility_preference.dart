c587c811450f6370e6984dc99d9265f4e4f60118
ArnabDasSOEN <arnabdas1234@hotmail.com>
2025-03-15 17:53:48 -0400

    added styles

--- frontend/lib/screens/indoor_accessibility/indoor_accessibility_preference.dart
+++ frontend/lib/screens/indoor_accessibility/indoor_accessibility_preference.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+//walking, escelator, elevator, stairs.
+//
 class IndoorAccessibilityPage extends StatefulWidget{
   const IndoorAccessibilityPage({
     super.key,
@@ -10,10 +12,26 @@ class IndoorAccessibilityPage extends StatefulWidget{
 class IndoorAccessibilityState extends State<IndoorAccessibilityPage>{
    bool isMobilityImpaired = false; //assume the user is not.
 
+   bool getMobilityStatus() {
+     return isMobilityImpaired; //will be used for testing purposes.
+   }
+
    Widget build(BuildContext context){
      return Column(
        children: [
+         const Text("PREFERENCES", style:TextStyle(
+           fontSize: 15
+         )),
+         const Divider(
+           color: Colors.grey, // Line color
+           thickness: 2,       // Line thickness
+           indent: 10,         // Left padding
+           endIndent: 10,      // Right padding
+         ),
          Text('requires mobility considerations: $isMobilityImpaired'),
+
+         
+
          Checkbox(
            value: isMobilityImpaired,
            onChanged: (bool? newValue) {
