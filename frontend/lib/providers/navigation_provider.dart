770df0ae8d110f3761bb9cc666e9d9bbbb06c908
sydneyC678 <sydcampbell@hotmail.com>
2025-03-19 18:29:09 -0400

    creating a navigation provider to allow for the nav bar to coordinate throughout the app when an index is changed

---
+++ frontend/lib/providers/navigation_provider.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+///This provider is used to manage the navigation state of the app.
+///It is used to keep track of the current selected index of the navigation bar.
+
+// State model for the navigation
+class NavigationState {
+  final int selectedIndex;
+
+  NavigationState({required this.selectedIndex});
+
+  NavigationState copyWith({int? selectedIndex}) {
+    return NavigationState(
+      selectedIndex: selectedIndex ?? this.selectedIndex,
+    );
+  }
+}
+
+// Notifier that handles the navigation state changes
+class NavigationNotifier extends StateNotifier<NavigationState> {
+  NavigationNotifier() : super(NavigationState(selectedIndex: 0));
+
+  void setSelectedIndex(int index) {
+    state = state.copyWith(selectedIndex: index);
+  }
+}
+
+// Provider that exposes the navigation state
+final navigationProvider =
+    StateNotifierProvider<NavigationNotifier, NavigationState>((ref) {
+  return NavigationNotifier();
+});
