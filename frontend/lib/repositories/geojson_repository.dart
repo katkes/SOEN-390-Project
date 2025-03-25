d677e52193885e15fe940bacbcb868a235b2762a
roburnto <58539522+roburnto@users.noreply.github.com>
2025-03-25 10:01:31 -0400

    Update geojson_repository.dart
    ensured the correct asset path was used

--- frontend/lib/repositories/geojson_repository.dart
+++ frontend/lib/repositories/geojson_repository.dart
@@ -9,11 +9,11 @@ class GeoJsonRepository {
       : loader = loader ?? GeoJsonLoader();
 
   Future<Map<String, dynamic>> loadBuildingList() =>
-      loader.load('assets/geojson/building_list.geojson');
+      loader.load('assets/geojson_files/building_list.geojson');
 
   Future<Map<String, dynamic>> loadBuildingBoundaries() =>
-      loader.load('assets/geojson/building_boundaries.geojson');
+      loader.load('assets/geojson_files/building_boundaries.geojson');
 
   Future<Map<String, dynamic>> loadCampusBoundaries() =>
-      loader.load('assets/geojson/campus.geojson');
+      loader.load('assets/geojson_files/campus.geojson');
 }
