fa24d28dbeee65e507742902eca2d29fc609c27f
Keshan Kathiripilay <ke_kathi@live.concordia.ca>
2025-04-06 16:57:24 -0400

    Update navigation_utils_test.dart
    fixed test

--- frontend/test/utils/navigation_utils_test.dart
+++ frontend/test/utils/navigation_utils_test.dart
@@ -1,17 +1,7 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:latlong2/latlong.dart';
-import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
 import 'package:soen_390/screens/indoor/mappedin_map_controller.dart';
-import 'package:soen_390/screens/waypoint/waypoint_selection_screens.dart';
-import 'package:soen_390/utils/navigation_utils.dart';
-import 'package:soen_390/providers/navigation_provider.dart';
-
-
 
 // Generate mocks
 @GenerateMocks([WidgetRef, MappedinMapController])
-void main() {
-
-}
+void main() {}
